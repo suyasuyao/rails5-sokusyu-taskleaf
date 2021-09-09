0bc58d87101a4cbdf0f674d1a8fb21a85b79dcb0
yasuyasu-ot <y.otsubo.dss@gmail.com>
2021-09-10 02:14:00 +0900

    Chapter3-3コントローラとビュー #7 routeファイルを修正

--- taskleaf/config/routes.rb
+++ taskleaf/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/new'
-  get 'tasks/edit'
+  resources :tasks
+  # トップページにtaskのindexページを表示
+  root to: 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
