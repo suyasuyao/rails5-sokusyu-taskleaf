3c632ca822f99f7083b596f27b72405e9b297702
yasuyasu-ot <y.otsubo.dss@gmail.com>
2022-01-26 05:38:55 +0900

    4-9 フィルタを使い重複を避ける #18 destroyメソッドの変数をインスタンス変数に変更

--- taskleaf/app/controllers/tasks_controller.rb
+++ taskleaf/app/controllers/tasks_controller.rb
@@ -42,9 +42,9 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    task = current_user.tasks.find(params[:id])
-    task.destroy
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+    @task = current_user.tasks.find(params[:id])
+    @task.destroy
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
   end
 
   private
